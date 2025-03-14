ccd852ceff6a5211f328a5b06d58522f1aa16ea2
Karl Dreher <karldreher@outlook.com>
2025-03-13 21:05:14 -0700

    fix: add repo

--- db/setup.sql
+++ db/setup.sql
@@ -1,6 +1,7 @@
 -- Commmit table contains all commit messages and their dates
 CREATE TABLE IF NOT EXISTS commits (
     id SERIAL PRIMARY KEY,
-    commit VARCHAR(255) NOT NULL,
+    commit VARCHAR(64) NOT NULL,
+    repo VARCHAR(255) NOT NULL,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
