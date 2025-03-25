0440965a2cd7ac9e115f6bfa2adcd5616abf4c6a
Karl Dreher <karldreher@outlook.com>
2025-03-24 19:25:25 -0700

    feat: cool computed field

--- db/setup.sql
+++ db/setup.sql
@@ -2,6 +2,7 @@
 CREATE TABLE IF NOT EXISTS commits (
     id SERIAL PRIMARY KEY,
     commit VARCHAR(64) NOT NULL,
+    conventional BOOLEAN GENERATED ALWAYS AS (commit ~ '^(feat|fix|docs|style|refactor|perf|test|chore)(\(.+\))?: .{1,50}') STORED,
     repo VARCHAR(255) NOT NULL,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
