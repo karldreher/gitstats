5a2a2c111caa4d274705a3a33535afd6fb0b0223
Karl Dreher <karldreher@outlook.com>
2025-04-09 19:49:40 -0700

    fix: exponential connect retry

--- server/internal/db/db.go
+++ server/internal/db/db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
@@ -17,15 +18,22 @@ var DB *sql.DB
 
 func Connect() {
 	var err error
-	DB, err = sql.Open("pgx", os.Getenv("DATABASE_URL"))
-	if err != nil {
-		log.Fatalf("Unable to connect to database: %v\n", err)
+	for i := range 5 {
+		DB, err = sql.Open("pgx", os.Getenv("DATABASE_URL"))
+		if err == nil {
+			pingErr := DB.Ping()
+			if pingErr == nil {
+				log.Println("Connected to database")
+				return
+			}
+			log.Printf("Ping attempt %d failed: %v\n", i+1, pingErr)
+			// Wait i * 2(i^2) seconds before retrying
+			time.Sleep(time.Duration(2*i*i) * time.Second)
+		} else {
+			log.Printf("Connection attempt %d failed: %v\n", i+1, err)
+		}
 	}
-	pingErr := DB.Ping()
-	if pingErr != nil {
-		log.Fatalf("Unable to ping database: %v\n", pingErr)
-	}
-	log.Println("Connected to database")
+	log.Fatalf("Unable to connect to database after 5 attempts: %v\n", err)
 
 }
 
