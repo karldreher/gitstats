40c927988e57f45fce6d4ebbcc44011e17edaad9
Karl Dreher <karldreher@outlook.com>
2025-04-09 20:23:22 -0700

    fix: client

--- client/simple.sh
+++ client/simple.sh
@@ -5,6 +5,6 @@ if [ -z "$commit" ] || [ -z "$repo" ]; then
     echo "Usage: $0 <commit> <repo>"
     exit 1
 fi
-# TODO: ENV that url
-curl -s -X POST -H "Content-Type: application/json" -d "{\"commit\": \"$commit\", \"repo\": \"$repo\"}" http://localhost:8000/api/v1/commit
+
+curl -s -X POST -H "Content-Type: application/json" -H "x-api-key: $GITSTATS_API_KEY" -d "{\"commit\": \"$commit\", \"repo\": \"$repo\"}" http://$GITSTATS_HOST/api/v1/commit
 
